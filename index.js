bed6277f369b3c8b8991c3682b019908da0257c4
dangtinh <caodangtinh04022003@gmail.com>
2024-12-03 01:13:53 +0700

    add: cors

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { exec } = require("child_process");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
